51ec21a32ff25dfd97e9671847f986c6928112e6
Bowen <bowen68@ucsb.edu>
2025-07-09 11:15:03 -0700

    train and val

--- val.sh
+++ val.sh
@@ -1,5 +1,5 @@
 
 CUDA_VISIBLE_DEVICES=5
 python val.py --data PATH_TO_run1_run2_yolo.yaml \
---weights PATH_TO_WEIGHTS --img 512 --task test  \
+--weights PATH_TO_WEIGHTS --img 512 --task test/val  \
 --name val --verbose --save-txt
